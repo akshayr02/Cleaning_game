2c3af59935fcd20d7b7efe82ba912d9bf009fb5a
Akshay Ramasubramanian <akshayr082002@gmail.com>
2025-06-06 18:54:56 -0400

    Actually fixed the camera now

--- Player/player.gd
+++ Player/player.gd
@@ -8,7 +8,6 @@ func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
 	if direction:
-		print(direction)
 		self.velocity = direction * SPEED
 	else:
 		self.velocity = Vector2.ZERO
--- camera.gd
+++ camera.gd
@@ -2,36 +2,32 @@ class_name Camera extends Camera2D
 
 @onready var tilemap: TileMapLayer = $"../../Test_tilemap"
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var used_rect = tilemap.get_used_rect() # The tile rectangle in grid space
-	#var tile_size = tilemap.tile_set.tile_size
-	var tile_size = 32
-	#
-	#var top_left = tilemap.map_to_local(used_rect.position)
-	#var bottom_right = tilemap.map_to_local(used_rect.position + used_rect.size)
-	#print(used_rect.position)
-	#print(used_rect.size)
-#
-	## Add tile size to bottom_right in case tilemap doesn't fully align
-	#bottom_right += Vector2(tile_size.x, tile_size.y)
-	
+	var tile_size = Vector2i(32, 16)
+
 	var bounds : Array[ Vector2 ] = []
+
 	bounds.append(
-		Vector2( used_rect.position * tile_size ) + tilemap.position
+		tilemap.map_to_local(used_rect.position - Vector2i(1,1)) #gives left corner coords (one left of it)
 	)
 	bounds.append(
-		Vector2( used_rect.end * tile_size/2 ) + tilemap.position
+		tilemap.map_to_local(used_rect.end) #gives right corner coords
 	)
-	print(bounds)
+	bounds.append(
+		tilemap.map_to_local(used_rect.position + Vector2i(used_rect.size[0], 0 )) #gives top corner coords
+	)
+	bounds.append(
+		tilemap.map_to_local(used_rect.end - Vector2i(used_rect.size[0], 0 )) #gives bottom corner coords
+	)
 	
 	if bounds == []:
 		return
 	limit_left = int( bounds[0].x )
-	limit_top = int( bounds[0].y )
 	limit_right = int( bounds[1].x )
-	limit_bottom = int( bounds[1].y )
+	limit_top = int(bounds[2].y)
+	limit_bottom = int( bounds[3].y)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
