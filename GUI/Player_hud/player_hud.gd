394d8d6a22b859ddb82ac96b939919e99cde018f
Akshay Ramasubramanian <akshayr082002@gmail.com>
2025-06-08 11:44:31 -0400

    Main menu and game over
    got the main menu and game over screen working, still need to finish updating the artwork

--- GUI/Player_hud/player_hud.gd
+++ GUI/Player_hud/player_hud.gd
@@ -5,48 +5,42 @@ var hearts : Array[HeartGUI] = []
 @onready var h_flow_container: HFlowContainer = $Control/HFlowContainer
 @onready var game_over: Control = $Control/GameOver
 @onready var retry: Button = $Control/GameOver/ColorRect/VBoxContainer/Retry
-@onready var main_menu: Button = $Control/GameOver/ColorRect/VBoxContainer/MainMenu
-@onready var animation_player: AnimationPlayer = $Control/GameOver/AnimationPlayer
-@onready var wave_label: Label = $Bot_panel/Wave_label
-@onready var enemies_label: Label = $Bot_panel/Enemies_label
+@onready var main_menu_button: Button = $Control/GameOver/ColorRect/VBoxContainer/MainMenu
+@onready var animation_player: AnimationPlayer = $Control/AnimationPlayer
+@onready var wave_label: Label = $Control/Bot_panel/Wave_label
+@onready var enemies_label: Label = $Control/Bot_panel/Enemies_label
+@onready var start: Custom_Button = $Control/MainMenu/Start
+@onready var main_menu: Control = $Control/MainMenu
+@onready var fade_to_black_all: ColorRect = $Control/FadeToBlackAll
 
 
 var is_paused : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-	#PauseMenu.shown.connect(hide_player_hud)
-	
+	reset()
+	show_main_menu()
+	#hide_game_over_screen()
 	for child in h_flow_container.get_children():
 		if child is HeartGUI:
 			hearts.append(child)
-			child.visible = false
-	#PauseMenu.hidden.connect(show_player_hud)
-	#reset()
+			child.visible = false	
 	
-	pass # Replace with function body.
-	
-#func show_player_hud() -> void:
-	#color_rect.visible = true
-	#label.visible = true
-	#
-#func hide_player_hud() -> void:
-	#color_rect.visible = false
-	#label.visible = false
 
 func reset() -> void:
-	animation_player.stop()
-	animation_player.seek(0, true)
-	hide_game_over_screen()
+	#animation_player.stop()
+	#animation_player.seek(0, true)
 	# Disconnect buttons to avoid duplicate connections
 	if retry.pressed.is_connected(load_game):
 		retry.pressed.disconnect(load_game)
-	if main_menu.pressed.is_connected(load_main_menu):
-		main_menu.pressed.disconnect(load_main_menu)
+	if main_menu_button.pressed.is_connected(load_main_menu):
+		main_menu_button.pressed.disconnect(load_main_menu)
+	if start.pressed.is_connected(start_game):
+		start.pressed.disconnect(start_game)
 	# Reconnect
 	retry.pressed.connect(load_game)
-	main_menu.pressed.connect(load_main_menu)
+	main_menu_button.pressed.connect(load_main_menu)
+	start.pressed.connect(start_game)
 	
 func update_hp(_hp : int, _max_hp : int) -> void:
 	update_max_hp(_max_hp)
@@ -84,18 +78,44 @@ func hide_game_over_screen() -> void:
 	pass
 
 func load_game() -> void:
-	await fade_to_black()
-	get_tree().reload_current_scene()
+	await fade_all_to_black()
 	hide_game_over_screen()
+	get_tree().reload_current_scene()
+	await fade_all_from_black()	
 	pass
 
 func load_main_menu() -> void:
-	await fade_to_black()
-	#load title screen
+	await fade_all_to_black()
+	hide_game_over_screen()
+	show_main_menu()
+	await fade_all_from_black()
 	pass
 
-func fade_to_black() -> bool:
-	animation_player.play("Fade_to_black")
+func show_main_menu() -> void:
+	main_menu.visible = true
+	game_over.mouse_filter = Control.MOUSE_FILTER_STOP
+	#get_tree().paused = true
+	start.grab_focus()
+
+func hide_main_menu() -> void:
+	main_menu.visible = false
+	
+func start_game() -> void:
+	await fade_all_to_black()
+	hide_main_menu()
+	get_tree().paused = false
+	reset()
+	get_tree().reload_current_scene()
+	await fade_all_from_black()
+	pass
+
+func fade_all_to_black() -> bool:
+	animation_player.play("Fade_all_to_black")
+	await animation_player.animation_finished
+	return true
+
+func fade_all_from_black() -> bool:
+	animation_player.play("Fade_all_from_black")
 	await animation_player.animation_finished
 	return true
 
